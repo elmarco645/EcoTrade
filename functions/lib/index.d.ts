f233b2e5d8a4e9ef0d45cb6e5b84d80fccbd27ff
elmarco645 <formgmr645@gmail.com>
2026-04-06 20:37:27 +0300

    refactor: Remove unused Firebase and reCAPTCHA configurations
    The `.gitignore` and `.firebaserc` files have been cleaned up by removing entries related to Firebase service account keys and project configurations.
    
    The `react-firebase-hooks` dependency has been removed from `package.json` and `package-lock.json`, as it was not being utilized.
    
    The `vite-env.d.ts` file has been updated to remove Firebase and reCAPTCHA environment variable declarations, as these configurations are no longer relevant.
    
    Finally, the `backend/firebase-admin.ts` and `backend/firestore.ts` files have been removed as they contained backend-specific Firebase initialization and Firestore data models that are no longer part of the project's architecture.

--- functions/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const api: import("firebase-functions/v2/https").HttpsFunction;
-export declare const onUserCreated: import("firebase-functions/core").CloudFunction<import("firebase-functions/v2/firestore").FirestoreEvent<import("firebase-functions/v2/firestore").QueryDocumentSnapshot | undefined, {
-    userId: string;
-}>>;
-export declare const scheduled: import("firebase-functions/v2/scheduler").ScheduleFunction;
